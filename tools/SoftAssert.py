e97b9f87f7fd5e3ebeeae623ddb1341a38402bba
vvoicu_dark <voicu.vac@evozon.com>
2016-08-02 14:39:38 +0300

    added soft assertions custom messages

--- tools/SoftAssert.py
+++ tools/SoftAssert.py
@@ -1,16 +1,23 @@
-
 failures = []
+
+
 class SoftAssert(object):
     def verfy_equals_true(self, message, expected, actual):
         if expected != actual:
-            failures.append(message)
+            failures.append(message + "Expected: '" + expected + "' - Actual: '" + actual + "'")
 
     def verfy_contains_true(self, message, expected, actual):
         if expected not in actual:
-            failures.append(message)
+            failures.append(message + "Expected: '" + expected + "' - Actual: '" + actual + "'")
 
     def return_failures_size(self):
         return len(failures)
 
     def return_failures_list(self):
         return failures
+
+
+if __name__ == "__main__":
+    SoftAssert().verfy_equals_true("message equals not as expected", "something", "other")
+    print SoftAssert().return_failures_size()
+    print SoftAssert().return_failures_list()
